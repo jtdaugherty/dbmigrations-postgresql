658fa4c4f557cfd1c7e7121c368eddabcba40d0b
Bastian Krol <bastian.krol@web.de>
2016-10-18 15:52:00 +0200

    separate types for configuration and executable parameters

--- programs/MooPostgreSQL.hs
+++ programs/MooPostgreSQL.hs
@@ -14,21 +14,16 @@ import Moo.Main
 
 main :: IO ()
 main = do
-  -- make sure the configuration validation does not complain about a missing
-  -- db type
-  setEnv "DBM_DATABASE_TYPE" "postgresql"
-
   args <- getArgs
   (_, opts, _) <- procArgs args
   conf <-
     loadConfiguration $ _configFilePath opts
   case conf of
-      Left e -> putStrLn e >> exitFailure
-      Right preliminaryConf -> do
-        let Left preliminaryDatabaseConf =  _backendOrConf preliminaryConf
-            connectionString = _connectionString preliminaryDatabaseConf
-        connection <- connectPostgreSQL connectionString
-        let backend = hdbcBackend connection
-            finalConf = preliminaryConf { _backendOrConf = Right backend }
-        mainWithConf args finalConf
+    Left e -> putStrLn e >> exitFailure
+    Right conf -> do
+      let connectionString = _connectionString conf
+      connection <- connectPostgreSQL connectionString
+      let backend = hdbcBackend connection
+          parameters = makeParameters conf backend
+      mainWithParameters args parameters
 
